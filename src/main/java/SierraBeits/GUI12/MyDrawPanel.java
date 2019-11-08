f419ee7ae566131c0ed1180955a0215923f80105
Kola Ra <santaramus@gmail.com>
2019-11-08 16:12:26 +0300

    add MiniMusicPlayerThree

--- src/main/java/SierraBeits/GUI12/MyDrawPanel.java
+++ src/main/java/SierraBeits/GUI12/MyDrawPanel.java
@@ -1,29 +1,37 @@
 package SierraBeits.GUI12;
 
+import javax.sound.midi.ControllerEventListener;
+import javax.sound.midi.ShortMessage;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
-public class MyDrawPanel extends JPanel {
+public class MyDrawPanel extends JPanel implements ControllerEventListener {
+    boolean msg = false;
 
-    public void paintComponent(Graphics g){
-        Random random = new Random();
-        Graphics2D g2d = (Graphics2D)g;
-
-        int red = random.nextInt(255);
-        int green = random.nextInt(255);
-        int blue = random.nextInt(255);
-        Color startColor = new Color(red,green,blue);
-
-        red = random.nextInt(255);
-        green = random.nextInt(255);
-        blue = random.nextInt(255);
-        Color endColor = new Color(red,green,blue);
-
-        GradientPaint gradient = new GradientPaint(70,70,startColor,150,150,endColor);
-        g2d.setPaint(gradient);
-        g2d.fillOval(90,75,100,100);
+    public void controlChange(ShortMessage event){
+        msg = true;
+        repaint();
     }
 
+    public void paintComponent(Graphics g){
+        if(msg){
+            Graphics2D g2 = (Graphics2D) g;
 
+            int red = (int)(Math.random()*250);
+            int green = (int)(Math.random()*250);
+            int blue = (int)(Math.random()*250);
+
+            g.setColor(new Color(red, green, blue));
+
+            int height = (int)((Math.random()*120)+10);
+            int width = (int)((Math.random()*120)+10);
+
+            int x = (int)((Math.random()*40)+10);
+            int y = (int)((Math.random()*40)+10);
+
+            g.fillRect(x,y,height,width);
+            msg = false;
+        }
+    }
 }
